9724ba5c8a92f1709887a7de9aea8ea7fa231017
Ronaldo Nascimento <lp.ronnaldo@gmail.com>
2025-03-29 08:44:07 -0300

    imagens adicionada ao content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,12 +3,25 @@ export const dataFake = [
    "id": "1",
     "title": "Sagrada Familia",
     "description": "sLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem harum fugiat optio culpa eveniet exercitationem natus error inventore rem neque, odio dolore quia minus. Vel minima adipisci recusandae nulla tenetur.",
-    "photoCover": "assets/img/sagradaFamilia-02.webp"
+    "photoCover": "../../assets/img/sagradaFamilia-02.webp"
   },
   {
     "id":"2",
     "title":"Ópera de Sydney",
     "description":" A majestosa Casa da Ópera de Sydney é um dos grandes símbolos da Austrália. Seu edifício fica na Baía de Sydney e foi projetado por Jorn Utzon, que ganhou uma competição de design local. Após 14 anos de construção, o edifício de arquitetura moderna inaugurado em 1973 é conhecimento ao redor do mundo, e hoje funciona como um centro cultural e artístico onde ocorrem muitos outros eventos além de apenas ópera.",
-    "photoCover":"assets/img/opera-sydney-02.webp"
+    "photoCover":"../../assets/img/opera-sydney-02.webp"
   },
+  {
+    "id":"3",
+    "title":"Taj Mahal",
+    "description":"O Taj Mahal, mausoléu situado na cidade indiana de Agra, é uma das Sete Maravilhas do Mundo Moderno. Esse edifício foi construído como uma prova de amor, pois foi um presente do imperador mongol Shan Jahan em homenagem à sua terceira esposa, chamada Aryumand Banu Begam.A construção do Taj Mahal durou cerca de 20 anos e empregou cerca de 20 mil operários. Esse mausoléu conta com características imponentes, como o material utilizado, de alto valor. O Taj Mahal tem grande importância histórica e econômica, visto que é um dos principais pontos turísticos do mundo.",
+    "photoCover":"../../assets/img/taj-mahal-02.webp"
+
+  },
+  {
+    "id":"4",
+    "title":"Ponte Golden Gate",
+    "description":"A ponte Golden Gate começou a ser construída no início de 1933 e foi finalizada quatro anos depois, em 1937. Ela foi arquitetada pelo engenheiro Joseph Strauss para conectar a baía de São Francisco – antes dela, só era possível atravessar a baía através de ferrys, que já estavam causando congestionamento nas águas.Diz a lenda que o nome Golden Gate veio porque um topógrafo achou que o estreito vão que a ponte cruza era parecido com um estreito de Istambul, o Chrysoceras (que significa “Chifre Dourado”).Quanto a cor, ela não é dourada e muito menos vermelha como muita gente pensa. A princípio ela seria pintada de amarelo e preto para facilitar a visualização das embarcações que passavam por ali, mas o tom “laranja internacional”, usado no início da construção para proteger o ferro, conquistou o coração da galera e acabou ficando. O laranja é tão forte, tão forte que chamar a ponte de vermelha ou avermelhada é quase regra.",
+    "photoCover":"../../assets/img/ponte-golden-gate-02.webp"
+  }
 ];
